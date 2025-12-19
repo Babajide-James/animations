099cf657933c562adf84d4b68359b64313acbca0
Babajide James <teestartaiwo121@gmail.com>
2025-12-19 17:43:50 +0100

    cat animation completed

--- lib/src/cat_animation.dart
+++ lib/src/cat_animation.dart
@@ -25,26 +25,27 @@ class CatAnimationState extends State<CatAnimation>
       duration: Duration(seconds: 1),
     );
     _catAnimation = Tween<double>(
-      begin: 0.0,
-      end: 100.0,
+      begin: 100.0,
+      end: 0.0,
     ).animate(CurvedAnimation(parent: _catController, curve: Curves.easeIn));
 
     _boxController = AnimationController(
       vsync: this,
-      duration: Duration(seconds: 1),
+      duration: Duration(milliseconds: 200),
     );
     _boxAnimation = Tween<double>(
       begin: -(pi / 4),
       end: -(pi / 3),
-    ).animate(CurvedAnimation(parent: _boxController, curve: Curves.bounceIn));
+    ).animate(CurvedAnimation(parent: _boxController, curve: Curves.linear));
 
     _textController = AnimationController(
       vsync: this,
-      duration: Duration(seconds: 1),
+      duration: Duration(milliseconds: 200),
     )..repeat(reverse: true);
-    _textAnimation = Tween(begin: 0.0, end: 20).animate(
-      CurvedAnimation(parent: _textController, curve: Curves.elasticIn),
-    );
+    _textAnimation = Tween(
+      begin: 0.0,
+      end: 20.0,
+    ).animate(CurvedAnimation(parent: _textController, curve: Curves.linear));
     _textController.repeat();
   }
 
@@ -58,90 +59,100 @@ class CatAnimationState extends State<CatAnimation>
 
   void onTap() {
     if (_catController.status == AnimationStatus.completed) {
+      _boxController.stop();
       _catController.reverse();
-      _boxController.reset();
-      _textController.reset();
-    } else if (_catController.status == AnimationStatus.dismissed) {
-      _catController.forward();
-      _boxController.repeat();
       _textController.repeat();
+    } else if (_catController.status == AnimationStatus.dismissed) {
+      _boxController.repeat();
+      _catController.forward();
+      _textController.stop();
     }
-    // if (_textController.status == AnimationStatus.completed) {}
+    // if (_textController.status == AnimationStatus.completed) {
+    // } else if (_textController.status == AnimationStatus.dismissed) {
+    //   _textController.forward();
+    // }
   }
 
   @override
   Widget build(context) {
     return MaterialApp(
-      home: SafeArea(
-        child: Scaffold(
-          body: Center(
-            child: GestureDetector(
-              onTap: onTap,
-              child: AnimatedBuilder(
-                animation: _catAnimation,
-                builder: (context, child) => Stack(
-                  clipBehavior: Clip.none,
-                  // margin: EdgeInsets.all(20),
-                  children: [
-                    Positioned(
-                      top: -130,
-                      height: 200,
-                      width: 200,
-                      child: Container(
-                        margin: EdgeInsets.only(top: _catAnimation.value),
-                        child: Image.network('https://i.imgur.com/QwhZRyL.png'),
-                      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Beautiful Cat Animation',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
+          backgroundColor: const Color.fromARGB(255, 3, 57, 101),
+        ),
+        body: Center(
+          child: GestureDetector(
+            onTap: onTap,
+            child: AnimatedBuilder(
+              animation: _catAnimation,
+              builder: (context, child) => Stack(
+                clipBehavior: Clip.none,
+                // margin: EdgeInsets.all(20),
+                children: [
+                  Positioned(
+                    top: -130,
+                    height: 200,
+                    width: 200,
+                    child: Container(
+                      margin: EdgeInsets.only(top: _catAnimation.value),
+                      child: Image.network('https://i.imgur.com/QwhZRyL.png'),
                     ),
-                    Container(
-                      height: 200,
-                      width: 200,
-                      decoration: BoxDecoration(color: Colors.blue),
-                    ),
+                  ),
+                  Container(
+                    height: 200,
+                    width: 200,
+                    decoration: BoxDecoration(color: Colors.blue),
+                  ),
 
-                    Positioned(
-                      bottom: 10,
-                      left: 30,
-                      right: 30,
-                      child: AnimatedBuilder(
-                        animation: _textAnimation,
-                        builder: (context, child) => Transform.translate(
-                          offset: Offset(_textAnimation.value, 0.0),
+                  Positioned(
+                    bottom: 10,
+                    left: 30,
+                    right: 30,
+                    child: AnimatedBuilder(
+                      animation: _textAnimation,
+                      builder: (context, child) => Transform.translate(
+                        offset: Offset(_textAnimation.value, 0.0),
 
-                          child: Center(
-                            child: Text(
-                              'Click Me!',
-                              style: TextStyle(
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold,
-                              ),
+                        child: Center(
+                          child: Text(
+                            'Click Me!',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
                         ),
                       ),
                     ),
-                    Transform.rotate(
-                      alignment: Alignment.topRight,
-                      angle: _boxAnimation.value,
-                      child: Container(
-                        margin: EdgeInsets.only(left: 190),
-                        height: 100,
-                        width: 10,
-                        decoration: BoxDecoration(color: Colors.blue),
-                      ),
+                  ),
+                  Transform.rotate(
+                    alignment: Alignment.topRight,
+                    angle: _boxAnimation.value,
+                    child: Container(
+                      margin: EdgeInsets.only(left: 190),
+                      height: 100,
+                      width: 10,
+                      decoration: BoxDecoration(color: Colors.blue),
                     ),
-                    Transform.rotate(
-                      alignment: Alignment.topLeft,
-                      angle: -(_boxAnimation.value),
-                      child: Container(
-                        // margin: EdgeInsets.only(left: 20),
-                        padding: EdgeInsets.all(10),
-                        height: 100,
-                        width: 10,
-                        decoration: BoxDecoration(color: Colors.blue),
-                      ),
+                  ),
+                  Transform.rotate(
+                    alignment: Alignment.topLeft,
+                    angle: -(_boxAnimation.value),
+                    child: Container(
+                      // margin: EdgeInsets.only(left: 20),
+                      padding: EdgeInsets.all(10),
+                      height: 100,
+                      width: 10,
+                      decoration: BoxDecoration(color: Colors.blue),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ),
